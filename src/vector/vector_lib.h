7823be8e9091c435c752501a23c0bd5ab630d0fb
Michael lemito <delovoy.lemit@gmail.com>
2024-03-09 20:58:59 +0300

    Aadd doxygen

--- src/vector/vector_lib.h
+++ src/vector/vector_lib.h
@@ -9,13 +9,65 @@ typedef struct Vector
     int size;
 } Vector;
 
+/**
+ * @brief Creates an empty vector
+ * 
+ * @param v Pointer to an object of Vector structure
+ * @param sz Initial size of the vector
+ */
 void Create(Vector *v, int sz);
+/**
+ * @brief Checks if the vector is empty
+ * 
+ * @param v Pointer to an object of Vector structure
+ * @return true - Empty
+ * @return false - Not empty
+ */
 bool Empty(Vector *v);
+/**
+ * @brief Get vector size
+ * 
+ * @param v Pointer to an object of Vector structure
+ * @return int 
+ */
 int Size(Vector *v);
+/**
+ * @brief Get object by id as data[i], but vector
+ * 
+ * @param v Pointer to an object of Vector structure
+ * @param i 
+ * @return int 
+ */
 int Load(Vector *v, int i);
+/**
+ * @brief Add object t to vector by index as data[i] = t
+ * 
+ * @param v Pointer to an object of Vector structure
+ * @param i index
+ * @param t object
+ */
 void Save(Vector *v, int i, int t);
+/**
+ * @brief Resize
+ * 
+ * @param v Pointer to an object of Vector structure
+ * @param sz 
+ */
 void Resize(Vector *v, int sz);
+/**
+ * @brief Checking for equivalence of vectors
+ * 
+ * @param l Pointer to an first object of Vector structure
+ * @param r Pointer to an second object of Vector structure
+ * @return true 
+ * @return false 
+ */
 bool Eq(Vector *l, Vector *r);
+/**
+ * @brief Destroys the vector
+ * 
+ * @param v Pointer to an object of Vector structure
+ */
 void Destroy(Vector *v);
 
 #endif // VECTOR_LAB_H
