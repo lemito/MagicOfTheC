b8ce52f49657abd53f6918d9b053fdfdbcf4ef94
Michael lemito <delovoy.lemit@gmail.com>
2024-04-20 13:30:03 +0300

    Тут будет сортировка слиянием

---
+++ src/sort/merge_sort.c
@@ -0,0 +1,13 @@
+//
+// Created by lemito on 4/20/24.
+//
+#include <stdio.h>
+#include "merge_sort.h"
+
+void merge(queue *q1, queue* q2, const queue* q){
+    printf("pass!");
+}
+
+void merge_sort(queue* q){
+    printf("pass!");
+}
