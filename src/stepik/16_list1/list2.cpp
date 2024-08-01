1c5e822ed291b7bb5c145ea11582798e438a030f
Michael lemito <delovoy.lemit@gmail.com>
2024-08-01 16:57:18 +0300

    Двусвязный список. Вроде многое исправил

--- src/stepik/16_list1/list2.cpp
+++ src/stepik/16_list1/list2.cpp
@@ -1,8 +1,9 @@
 /**
  * Двусвязный список
- * Цели - такие же, как и у односвязного, только чуть круче, больше функций и возможностей
- * Плохо приспособлен для C
+ * Цели - такие же, как и у односвязного, только чуть круче, больше функций и
+ * возможностей Плохо приспособлен для C
  */
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 typedef int T;
@@ -20,7 +21,22 @@ struct Node {
    * @param prev
    */
   Node(Node* next, T data, Node* prev) : data(data), next(next), prev(prev) {};
+
+  [[maybe_unused]] [[nodiscard]] inline T get_data() const {
+    return this->data;
+  }
+
+  [[maybe_unused]] inline void set_data(T obj) { this->data = obj; }
+
+  [[maybe_unused]] [[nodiscard]] Node* Next() const {
+    return this->next;
+  }
+
+  [[maybe_unused]] [[nodiscard]] Node* Prev() const {
+    return this->prev;
+  }
 };
+
 struct List {
   // всегда околонулевой; схема -
   // https://stepik.org/media/attachments/lesson/308797/dlist_barier_model.png
@@ -37,7 +53,7 @@ struct List {
 
   void Print() const {
     for (Node* p = this->root->next; p != this->root; p = p->next) {
-      printf("%d ", p->data);
+      printf("%d ", p->get_data());
     }
     puts("");
   }
@@ -68,7 +84,8 @@ struct List {
   void Insert(T obj, size_t pos) {
     if (pos == 0) {
       Node* new_node = new Node(this->root->next, obj, this->root);
-      this->root->next = new_node;
+      this->root->next = this->root->prev = new_node;
+      ++this->size;
       return;
     }
     Node* cur = this->root;
@@ -77,7 +94,11 @@ struct List {
     }
     Node* new_node = new Node(cur->next, obj, cur);
     cur->next = new_node;
-    cur->next->prev = new_node;
+    //
+    new_node->prev = cur;
+    if (cur->next != NULL) {
+      cur->next->prev = new_node;
+    }
     ++this->size;
   }
 
@@ -93,7 +114,7 @@ struct List {
   }
 
   [[nodiscard]] bool IsEmpty() const {
-    return this->root->next == this->root && this->root->prev == this->root;
+    return this->root->next == this->root && this->root->prev == this->root || this->size == 0;
   }
 
   /**
@@ -146,22 +167,68 @@ struct List {
     this->size = 0;
   }
 
-  ~List(){
-    this->Destroy();
+  void swap(Node* a, Node* b) {
+    if (a == b) return;
+
+    Node* a_prev = a->prev;
+    Node* a_next = a->next;
+    Node* b_prev = b->prev;
+    Node* b_next = b->next;
+
+    if (a_next == b) {
+      Node* tmp = b->next;
+      a->next = tmp;
+      if (tmp != NULL) tmp->prev = a;
+
+      b->next = a;
+      b->prev = a_prev;
+
+      a->prev = b;
+      if (a_prev != NULL) a_prev->next = b;
+    } else if (b_next == a) {
+      Node* tmp = a->next;
+      b->next = tmp;
+      if (tmp != NULL) tmp->prev = b;
+
+      a->next = b;
+      a->prev = b_prev;
+
+      b->prev = a;
+      if (b_prev != NULL) b_prev->next = a;
+    } else {
+      a->next = b_next;
+      a->prev = b_prev;
+      if (b_next != NULL) b_next->prev = a;
+      if (b_prev != NULL) b_prev->next = a;
+
+      b->next = a_next;
+      b->prev = a_prev;
+      if (a_next != NULL) a_next->prev = b;
+      if (a_prev != NULL) a_prev->next = b;
+    }
   }
+
+    ~List() { this->Destroy(); }
 };
 
 int main() {
   List ll;
   ll.Push_back(22);
   ll.Push_back(2222);
-  //  Node* dvatsatdva = ll.l_search(22);
+  ll.Push_back(5);
+
   //  ll.Insert(5, dvatsatdva);
-  ll.Insert(5, (size_t)2);
-  ll.Remove(22);
+  //  ll.Insert(5, (size_t)2);
+  ll.Push_back(4);
+  ll.Push_back(8);
+  Node* dvatsatdva22 = ll.l_search(2222);
+  Node* dvatsatdva = ll.l_search(4);
+  //  ll.Remove(22);
   ll.Print();
-  printf("%d", ll.search(1)->data);
-  printf("\t%d\t", ll.IsEmpty());
-  ll.Destroy();
+  //  printf("%d", ll.search(1)->data);
+  //  printf("\t%d\t", ll.IsEmpty());
+  ll.swap(dvatsatdva, dvatsatdva22);
+  ll.Print();
+  //  ll.Destroy();
   return 0;
 }
