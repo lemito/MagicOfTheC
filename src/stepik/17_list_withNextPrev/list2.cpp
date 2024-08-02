cdc085dada91efbb2f6c20adb7e335d95b6ac2f4
Michael lemito <delovoy.lemit@gmail.com>
2024-08-02 17:43:22 +0300

    Теперь умные указатели

--- src/stepik/17_list_withNextPrev/list2.cpp
+++ src/stepik/17_list_withNextPrev/list2.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
+#include <utility>
 
 #define attr_MU [[maybe_unused]]
 #define attr_ND [[nodiscard]]
@@ -18,10 +20,10 @@ typedef int T;
 struct Node {
   T data;
   /**
-  * TODO: попробовать умные указатели
-  */
-  struct Node* next;
-  struct Node* prev;
+   * Теперь умные указатели
+   */
+  std::shared_ptr<Node> next;
+  std::shared_ptr<Node> prev;
 
   Node() : data(0), next(NULL), prev(NULL) {};
   /**
@@ -30,42 +32,53 @@ struct Node {
    * @param data
    * @param prev
    */
-  Node(Node* next, T data, Node* prev) : data(data), next(next), prev(prev) {};
+  Node(std::shared_ptr<Node> next, T data, std::shared_ptr<Node> prev)
+      : data(data), next(std::move(next)), prev(std::move(prev)) {};
 
   attr_ND inline T get_data() const { return this->data; }
 
   attr_MU inline void set_data(T obj) { this->data = obj; }
 
-  attr_MU attr_ND Node* Next() const { return this->next; }
+  attr_MU attr_ND std::shared_ptr<Node> Next() const { return this->next; }
 
-  attr_MU attr_ND Node* Prev() const { return this->prev; }
+  attr_MU attr_ND std::shared_ptr<Node> Prev() const { return this->prev; }
 };
 
 struct List {
   // всегда околонулевой; схема -
   // https://stepik.org/media/attachments/lesson/308797/dlist_barier_model.png
-  Node* root;
+  //  Node* root;
+  std::shared_ptr<Node> root;
   size_t size;
 
   List() {
-    this->root = new Node;
+    //    this->root = new Node;
+    this->root = std::make_shared<Node>();
     this->root->prev = this->root->next = this->root;
     this->root->data = 0;
     this->size = 0;
   };
-  List(Node* r) : root(r), size(1) {};
+  explicit List(std::shared_ptr<Node> r) : root(std::move(r)), size(1) {};
 
   void Print() const {
-    for (Node* p = this->root->next; p != this->root; p = p->next) {
+    //    for (Node* p = this->root->next; p != this->root; p = p->next) {
+    //      printf("%d ", p->get_data());
+    //    }
+    auto p = this->root->next;
+    while (p != this->root) {
       printf("%d ", p->get_data());
+      p = p->next;
     }
     puts("");
   }
 
   void Push_back(T obj) {
-    Node* head = new Node(this->root, obj, this->root->prev);
+    //    Node* head = new Node(this->root, obj, this->root->prev);
+    auto head = std::make_shared<Node>(this->root, obj, this->root->prev);
     this->root->prev->next = head;
     this->root->prev = head;
+    head->next = this->root;
+    head->prev = this->root->prev;
     ++this->size;
   }
   /**
@@ -73,11 +86,12 @@ struct List {
    * @param obj
    * @param it
    */
-  void Insert(T obj, Node* it) {
-    Node* new_node = new Node(it->next, obj, it);
+  void Insert(T obj, const std::shared_ptr<Node>& it) {
+    //    Node* new_node = new Node(it->next, obj, it);
+    auto new_node = std::make_shared<Node>(it->next, obj, it);
     it->next = new_node;
     it->next->prev = new_node;
-    this->root->prev = new_node;
+    if (new_node->next != NULL) this->root->prev = new_node;
     ++this->size;
   }
   /**
@@ -87,20 +101,22 @@ struct List {
    */
   void Insert(T obj, size_t pos) {
     if (pos == 0) {
-      Node* new_node = new Node(this->root->next, obj, this->root);
-      this->root->next = this->root->prev = new_node;
+      auto new_node = std::make_shared<Node>(this->root->next, obj, this->root);
+      this->root->next = new_node;
+      this->root->prev = new_node;
+      new_node->next = this->root;
+      new_node->prev = this->root;
       ++this->size;
       return;
     }
-    Node* cur = this->root;
+    auto cur = this->root;
     for (size_t cnt = 0; cnt < pos; ++cnt) {
       cur = cur->next;
     }
-    Node* new_node = new Node(cur->next, obj, cur);
+    auto new_node = std::make_shared<Node>(cur->next, obj, cur);
     cur->next = new_node;
-    //
     new_node->prev = cur;
-    if (cur->next != NULL) {
+    if (cur->next != nullptr) {
       cur->next->prev = new_node;
     }
     ++this->size;
@@ -110,15 +126,15 @@ struct List {
    * Удаление элемента по итератору
    * @param it
    */
-  void Remove(Node* it) {
+  void Remove(const std::shared_ptr<Node>& it) {
     it->prev->next = it->next;
     it->next->prev = it->prev;
     --this->size;
-    delete it;
   }
 
   attr_ND bool IsEmpty() const {
-    return this->root->next == this->root && this->root->prev == this->root ||
+    return this->root->next == this->root &&
+               this->root->prev == this->root ||
            this->size == 0;
   }
 
@@ -127,8 +143,8 @@ struct List {
    * @param obj
    * @return
    */
-  attr_ND Node* l_search(T obj) const {
-    Node* res = this->root->next;
+  attr_ND std::shared_ptr<Node> l_search(T obj) const {
+    auto res = this->root->next;
     while (res != this->root) {
       if (res->data == obj) {
         return res;
@@ -143,8 +159,8 @@ struct List {
    * @param ix
    * @return Node*
    */
-  attr_ND Node* search(size_t ix) const {
-    Node* cur = this->root;
+  attr_ND std::shared_ptr<Node> search(size_t ix) const {
+    auto cur = this->root;
     for (size_t cnt = 0; cnt < ix + 1; ++cnt) {
       cur = cur->next;
     }
@@ -157,31 +173,31 @@ struct List {
    * @param obj
    */
   void Remove(T obj) {
-    Node* it = l_search(obj);
+    auto it = l_search(obj);
     Remove(it);
     --this->size;
   }
 
   void Destroy() {
-    Node* cur = this->root->prev;
+    auto cur = this->root->prev;
     while (!this->IsEmpty()) {
-      Node* tmp = cur;
+      auto tmp = cur;
       Remove(tmp);
       cur = cur->prev->next;
     }
     this->size = 0;
   }
 
-  void swap(Node* a, Node* b) {
+  void swap(const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b) {
     if (a == b) return;
 
-    Node* a_prev = a->prev;
-    Node* a_next = a->next;
-    Node* b_prev = b->prev;
-    Node* b_next = b->next;
+    auto a_prev = a->prev;
+    auto a_next = a->next;
+    auto b_prev = b->prev;
+    auto b_next = b->next;
 
     if (a_next == b) {
-      Node* tmp = b->next;
+      auto tmp = b->next;
       a->next = tmp;
       if (tmp != NULL) tmp->prev = a;
 
@@ -191,7 +207,7 @@ struct List {
       a->prev = b;
       if (a_prev != NULL) a_prev->next = b;
     } else if (b_next == a) {
-      Node* tmp = a->next;
+      auto tmp = a->next;
       b->next = tmp;
       if (tmp != NULL) tmp->prev = b;
 
@@ -226,14 +242,14 @@ int main() {
   //  ll.Insert(5, (size_t)2);
   ll.Push_back(4);
   ll.Push_back(8);
-  Node* dvatsatdva22 = ll.l_search(2222);
-  Node* dvatsatdva = ll.l_search(4);
+  //  Node* dvatsatdva22 = ll.l_search(2222);
+  //  Node* dvatsatdva = ll.l_search(4);
   //  ll.Remove(22);
   ll.Print();
   //  printf("%d", ll.search(1)->data);
   //  printf("\t%d\t", ll.IsEmpty());
-  ll.swap(dvatsatdva, dvatsatdva22);
-  ll.Print();
+  //  ll.swap(dvatsatdva, dvatsatdva22);
+  //  ll.Print();
   //  ll.Destroy();
   return 0;
 }
