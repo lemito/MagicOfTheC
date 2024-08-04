246c264bc0c4aeca6eeca5af5ce24655711c02dc
Michael lemito <delovoy.lemit@gmail.com>
2024-08-04 20:17:43 +0300

    Задача решена
    Даже две - tree_4 и tree_3

---
+++ src/stepik/18_tree/tree_3.c
@@ -0,0 +1,28 @@
+/**
+ * Реализуйте бинарное дерево поиска для целых чисел. Программа получает на вход
+ * последовательность целых чисел и строит из них дерево. Элементы в деревья
+ * добавляются в соответствии с результатом поиска их места. Если элемент уже
+ * существует в дереве, добавлять его не надо. Балансировка дерева не
+ * производится.
+ */
+#include <stdio.h>
+
+#include "18_ctree_lib.h"
+
+#define MAX(a, b) (a > b ? a : b)
+
+int tree_depth(Node* tree) {
+  if (tree == NULL) return 0;
+  int l_d = tree_depth(tree->left);
+  int r_d = tree_depth(tree->right);
+  return MAX(l_d, r_d) + 1;
+}
+
+int main(void) {
+  int inpt;
+  while (scanf("%d", &inpt) == 1 && inpt != 0) {
+    tree = tree_add(tree, inpt);
+  }
+  printf("%d", tree_depth(tree));
+  return 0;
+}
---
+++ src/stepik/18_tree/tree_4.cpp
@@ -0,0 +1,25 @@
+/**
+ * Реализуйте бинарное дерево поиска для целых чисел. Программа получает на вход
+ * последовательность целых чисел и строит из них дерево. Элементы в деревья
+ * добавляются в соответствии с результатом поиска их места. Если элемент уже
+ * существует в дереве, добавлять его не надо. Балансировка дерева не
+ * производится.
+ */
+#include "18_ctree_lib.h"
+
+void tree_inline_print(Node* tree) {
+  if (tree == NULL) return;
+  if (tree->left == NULL && tree->right == NULL) printf("%d ", tree->data);
+  tree_inline_print(tree->left);
+
+  tree_inline_print(tree->right);
+}
+
+int main() {
+  int inpt;
+  while (scanf("%d", &inpt) == 1 && inpt != 0) {
+    tree = tree_add(tree, inpt);
+  }
+  tree_inline_print(tree);
+  return 0;
+}
