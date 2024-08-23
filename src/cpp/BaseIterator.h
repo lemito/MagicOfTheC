8e139bba98ff3ba8177d8211c342d2600c1ba303
Michael Tropin <mihtrop2005@yandex.ru>
2024-08-23 12:22:04 +0300

    какая-то фигня

---
+++ src/cpp/BaseIterator.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <memory>
+
+namespace BaseIteratorNS {
+template <typename T>
+class BaseIterator {
+ public:
+  BaseIterator() = default;
+  virtual ~BaseIterator() = default;
+
+  bool operator==(const BaseIterator &other) const {
+    return _data.get() == other._data.get();
+  }
+
+  bool operator!=(const BaseIterator &other) const { return !(*this == other); }
+
+  virtual T &operator*();
+  virtual const T &operator*() const;
+
+  virtual BaseIterator<T> *Next();
+  virtual BaseIterator<T> *Prev();
+
+  virtual BaseIterator *operator++() { return nullptr; }
+  virtual BaseIterator *operator++(int) { return nullptr; }
+  virtual BaseIterator *operator--() { return nullptr; }
+  virtual BaseIterator *operator--(int) { return nullptr; }
+
+  void PushData(T obj);
+  T GetData();
+
+ protected:
+  std::shared_ptr<T> _data = std::make_shared<T>();
+};
+//
+// template <typename T>
+// inline bool BaseIterator<T>::operator==(const BaseIterator &other) {
+//   return (*this == other);
+// }
+//
+// template <typename T>
+// inline bool BaseIterator<T>::operator!=(const BaseIterator &other) {
+//   return (*this == other);
+// }
+
+template <typename T>
+inline T &BaseIterator<T>::operator*() {
+  return *_data;
+}
+
+template <typename T>
+inline const T &BaseIterator<T>::operator*() const {
+  return *_data;
+}
+
+template <typename T>
+inline BaseIterator<T> *BaseIterator<T>::Next() {
+  return nullptr;
+}
+
+template <typename T>
+inline BaseIterator<T> *BaseIterator<T>::Prev() {
+  return nullptr;
+}
+//
+// template <typename T>
+// inline BaseIterator<T>* BaseIterator<T>::GetCur() {
+//   return this->__current;
+// }
+template <typename T>
+inline void BaseIterator<T>::PushData(T obj) {
+  _data = std::make_shared<T>(obj);
+}
+template <typename T>
+inline T BaseIterator<T>::GetData() {
+  return *(this->_data);
+}
+}  // namespace BaseIteratorNS
---
+++ src/cpp/LVecor.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+
+#include "BaseIterator.h"
+namespace LVector {
+template <typename T>
+class LVectorIterator final : public BaseIteratorNS::BaseIterator<T> {
+ public:
+  explicit LVectorIterator(T* data) : _data(data) {}
+  ~LVectorIterator() override;
+
+  LVectorIterator* Next() override;
+  LVectorIterator* Prev() override;
+
+  T& operator*() override;
+  const T& operator*() const override;
+
+  LVectorIterator* operator++() override;
+  LVectorIterator* operator++(int) override;
+  LVectorIterator* operator--() override;
+  LVectorIterator* operator--(int) override;
+
+  bool operator==(const LVectorIterator& other) const;
+  bool operator!=(const LVectorIterator& other) const;
+
+ private:
+  T* _data;
+};
+
+template <typename T>
+LVectorIterator<T>::~LVectorIterator() {
+  delete _data;
+}
+
+template <typename T>
+LVectorIterator<T>* LVectorIterator<T>::Next() {
+  return new LVectorIterator<T>(_data + 1);
+}
+
+template <typename T>
+LVectorIterator<T>* LVectorIterator<T>::Prev() {
+  return new LVectorIterator<T>(_data - 1);
+}
+
+template <typename T>
+inline T& LVectorIterator<T>::operator*() {
+  return *_data;
+}
+
+template <typename T>
+inline const T& LVectorIterator<T>::operator*() const {
+  return *_data;
+}
+
+template <typename T>
+LVectorIterator<T>* LVectorIterator<T>::operator++() {
+  ++_data;
+  return this;
+}
+
+template <typename T>
+LVectorIterator<T>* LVectorIterator<T>::operator++(int) {
+  LVectorIterator* tmp = new LVectorIterator<T>(_data);
+  ++_data;
+  return tmp;
+}
+
+template <typename T>
+LVectorIterator<T>* LVectorIterator<T>::operator--() {
+  --_data;
+  return this;
+}
+
+template <typename T>
+LVectorIterator<T>* LVectorIterator<T>::operator--(int) {
+  LVectorIterator<T>* tmp = new LVectorIterator<T>(_data);
+  --_data;
+  return tmp;
+}
+
+template <typename T>
+bool LVectorIterator<T>::operator==(const LVectorIterator& other) const {
+  return _data == other._data;
+}
+
+template <typename T>
+bool LVectorIterator<T>::operator!=(const LVectorIterator& other) const {
+  return !(*this == other);
+}
+
+template <typename T>
+class LVector {
+ public:
+  LVector() : _size(0), _capacity(1) { _data = new T[_capacity]; }
+  ~LVector() { delete[] _data; }
+
+  void push_back(T obj);
+
+  size_t size();
+
+  T operator[](size_t index) const { return _data[index]; }
+
+  LVectorIterator<T> begin() const { return LVectorIterator<T>(_data); }
+  LVectorIterator<T> end() const { return LVectorIterator<T>(_data + _size); }
+
+ private:
+  size_t _size;
+  size_t _capacity;
+  T* _data;
+};
+
+template <typename T>
+void LVector<T>::push_back(T obj) {
+  _data[_size] = obj;
+  ++_size;
+  ++_capacity;
+}
+template <typename T>
+size_t LVector<T>::size() {
+  return this->_size;
+}
+
+}  // namespace LVector
+
+void Printer_by_ix(LVector::LVector<int>& ll) {
+  const size_t ss = ll.size();
+  for (size_t i = 0; i < ss; ++i) {
+    std::cout << ll[i] << ' ';
+  }
+  std::cout << '\n';
+}
+
+void Printer_by_it(LVector::LVector<int>& ll) {
+  for (auto it = ll.begin(); it != ll.end(); ++it) {
+    std::cout << *it << ' ';
+  }
+  std::cout << '\n';
+}
+
+int main() {
+  LVector::LVector<int> ll{};
+  ll.push_back(1);
+  ll.push_back(2);
+  ll.push_back(5);
+  // Printer_by_ix(ll);
+  Printer_by_it(ll);
+  return 0;
+}
