aa253def2ed167b7760b5ef3f058800599fe073e
Michael Tropin <mihtrop2005@yandex.ru>
2024-08-24 14:59:21 +0300

    Вектор чуть более итерабелен

--- src/cpp/LVecor.cpp
+++ src/cpp/LVecor.cpp
@@ -8,7 +8,8 @@ namespace LVector {
 template <typename T>
 class LVectorIterator final : public BaseIteratorNS::BaseIterator<T> {
  public:
-  explicit LVectorIterator(T* data) : _data(data) {}
+  LVectorIterator() = default;
+  explicit LVectorIterator(T data, size_t ix) : _data(data), _ix(ix) {}
   ~LVectorIterator() override = default;
 
   LVectorIterator* Next() override;
@@ -22,62 +23,67 @@ class LVectorIterator final : public BaseIteratorNS::BaseIterator<T> {
   LVectorIterator* operator--() override;
   LVectorIterator* operator--(int) override;
 
+  // LVectorIterator& operator=();
+
   bool operator==(const LVectorIterator& other) const;
   bool operator!=(const LVectorIterator& other) const;
 
+  T data() { return _data; }
+  void set_data(T obj) { _data = obj; }
+
+  size_t ix() { return _ix; }
+  void set_ix(size_t ix) { _ix = ix; }
+
  private:
-  T* _data;
+  size_t _ix;
+  T _data{};
+  LVectorIterator* _next = nullptr;
+  LVectorIterator* _prev = nullptr;
 };
 
 // template <typename T>
 // LVectorIterator<T>::~LVectorIterator() {
-//   delete _data;
+//   delete[] _data;
 // }
 
 template <typename T>
 LVectorIterator<T>* LVectorIterator<T>::Next() {
-  return new LVectorIterator<T>(_data + 1);
+  return _next;
 }
 
 template <typename T>
 LVectorIterator<T>* LVectorIterator<T>::Prev() {
-  return new LVectorIterator<T>(_data - 1);
+  return _prev;
 }
 
 template <typename T>
 inline T& LVectorIterator<T>::operator*() {
-  return *_data;
+  return _data;
 }
 
 template <typename T>
 inline const T& LVectorIterator<T>::operator*() const {
-  return *_data;
+  return _data;
 }
 
 template <typename T>
 LVectorIterator<T>* LVectorIterator<T>::operator++() {
-  ++_data;
-  return this;
+  return _next;
 }
 
 template <typename T>
 LVectorIterator<T>* LVectorIterator<T>::operator++(int) {
-  LVectorIterator* tmp = new LVectorIterator<T>(_data);
-  ++_data;
-  return tmp;
+  return _next;
 }
 
 template <typename T>
 LVectorIterator<T>* LVectorIterator<T>::operator--() {
-  --_data;
-  return this;
+  return _prev;
 }
 
 template <typename T>
 LVectorIterator<T>* LVectorIterator<T>::operator--(int) {
-  LVectorIterator<T>* tmp = new LVectorIterator<T>(_data);
-  --_data;
-  return tmp;
+  return _prev;
 }
 
 template <typename T>
@@ -93,56 +99,60 @@ bool LVectorIterator<T>::operator!=(const LVectorIterator& other) const {
 template <typename T>
 class LVector {
  public:
-  LVector() : _size(0), _capacity(1) { _data = new T[_capacity]; }
+  LVector() : _size(0), _capacity(1) {
+    _data = new LVectorIterator<T>[_capacity];
+  }
   ~LVector() { delete[] _data; }
 
   void push_back(T obj);
 
-  size_t size();
+  const size_t size() const;
 
   T operator[](size_t index) {
     if (index > _size) throw std::out_of_range("Index not found");
-    return _data[index];
+    return _data[index].data();
   }
 
   T operator[](size_t index) const {
     if (index > _size) throw std::out_of_range("Index not found");
-    return _data[index];
+    return _data[index].data();
   }
 
-  LVectorIterator<T> begin() const { return LVectorIterator<T>(_data); }
-  LVectorIterator<T> end() const { return LVectorIterator<T>(_data + _size); }
+  LVectorIterator<T> begin() const { return _data[0]; }
+  LVectorIterator<T> end() const { return _data[_size]; }
 
  private:
-  size_t _size;
-  size_t _capacity;
-  T* _data;
+  size_t _size;      // размер
+  size_t _capacity;  // капасити :)
+  // T* _data;
+  LVectorIterator<T>* _data;  // массив итераторов
 
   /**
    * Возвращает новый массив данных с больших/меньшим кол-вом памяти
    * @param new_size
    * @return
    */
-  T* _resize(const size_t new_size) {
-    T* new_data = new T[new_size];
-    // std::copy(_data, _data +  MIN(_size, new_size), new_data);
-    memmove(new_data, _data, MAX(_capacity, _size));
-    // _size = new_size;
+  LVectorIterator<T>* _resize(const size_t new_size) {
+    LVectorIterator<T>* new_data = new LVectorIterator<T>[new_size];
+    memcpy(new_data, _data, _size * sizeof(LVectorIterator<T>));
+    delete[] _data;
     return new_data;
   }
 };
 
 template <typename T>
 void LVector<T>::push_back(T obj) {
-  _data[_size] = obj;
+  _data[_size].set_data(obj);
+  _data[_size].set_ix(_size);
   ++_size;
   if (_capacity <= _size) {
     _capacity += 10;
     _data = _resize(_capacity);
   }
 }
+
 template <typename T>
-size_t LVector<T>::size() {
+const size_t LVector<T>::size() const {
   return this->_size;
 }
 
@@ -168,8 +178,8 @@ int main() {
   ll.push_back(1);
   ll.push_back(2);
   ll.push_back(5);
-  // Printer_by_ix(ll);
-  Printer_by_it(ll);
+  Printer_by_ix(ll);
+  // Printer_by_it(ll);
   try {
     int error = ll[10];
     std::cout << error << std::endl;
